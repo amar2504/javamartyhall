74ef47096a0d089d2be2628c619a6616735e8a04
amar2504 <amar.kumar2504@gmail.com>
2019-06-02 15:02:43 +0530

    Add files via upload
    These two file are part of Collection exerise and contains the code for list add and get.

--- Circle.java
+++ Circle.java
@@ -1,32 +1,17 @@
-package oppexercise2;
 
 public class Circle {
-	private double radius;
 	
-	/** Gives the radius.
-	 * @return gives the radius of the circle.
-	 */
+	private double radius;
 
-	public double getRadius() {
-		return radius;
-	}
-
-
-	public void setRadius(double radius) {
+	public Circle(double radius) {
 		this.radius = radius;
-	}
-	/**Area of the circle
-	 * Area of the given circle
-	 */
-	public void getArea()
-	{
-		System.out.println(Math.PI * radius *radius);
 	}
 	
 	@Override
 	public String toString() {
-		return ("Circle's Radius is " + this.radius);
-		
+		return ("Circle with radius = " + this.radius);
 	}
+	
+
 
 }
--- CircleTester.java
+++ CircleTester.java
@@ -1,10 +1,27 @@
-package oppexercise2;
+import java.util.*; 
 
 public class CircleTester {
-public static void main(String[] args) {
-	Circle c = new Circle();
-	c.setRadius(7.0);
-	c.getArea();
-	System.out.println(c);
+	
+	public static void main(String[] args) {
+	
+	List <Circle> listofCircle = new LinkedList<>();
+	
+	while(true)
+	{
+		double r = Math.random();
+		if	(r>=0.01)
+		{
+			Circle c = new Circle(r);
+			listofCircle.add(c);
+		}
+		else
+			break;
 	}
+	
+	for (int i =0; i<listofCircle.size();i++)
+	{
+		
+		System.out.println(listofCircle.get(i));
+	}
+ }
 }
